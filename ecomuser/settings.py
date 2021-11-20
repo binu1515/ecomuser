cd86bc79c2bc00c216c1a228d0f7f7af985e09d9
Your Name <you@example.com>
2021-11-20 09:40:05 -0800

    push

--- ecomuser/settings.py
+++ ecomuser/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-4w0=n$!-*gf^mmfu1)jrenju$mqc-kczd-ej4sa5pjfgl*tyof
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
 ALLOWED_HOSTS = ['plantifyin.herokuapp.com']
 
 
@@ -125,15 +125,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 #STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+#STATICFILES_DIRS = [
+    #os.path.join(BASE_DIR, 'static')
 
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+#]
+#STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'shop/') 
