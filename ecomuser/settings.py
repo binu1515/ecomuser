da6f94437599bff1f7f08ccbbf227b292b6bea6b
Your Name <you@example.com>
2021-11-20 10:00:15 -0800

    push

--- ecomuser/settings.py
+++ ecomuser/settings.py
@@ -135,8 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'shop/') 
-MEDIA_URL = '/shop/'
+#MEDIA_ROOT =  os.path.join(BASE_DIR, 'shop/') 
+#MEDIA_URL = '/shop/'
 
 CART_SESSION_ID = 'cart'
 
