093786e9aa974e1bf608d862fd52987de7b65618
Your Name <you@example.com>
2021-11-24 06:19:45 -0800

    sqllite add

--- ecomuser/settings.py
+++ ecomuser/settings.py
@@ -81,11 +81,8 @@ WSGI_APPLICATION = 'ecomuser.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'ceaser',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
